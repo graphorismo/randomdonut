f40705ac1e2d11b4956b49c7a60f042c9a77d9c2
graphorismo <graphorismo@gmail.com>
2022-10-15 09:13:43 +0300

    Set up image loading with coil lib

--- app/src/main/java/ru/graphorismo/randomdonut/ui/MainActivity.kt
+++ app/src/main/java/ru/graphorismo/randomdonut/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
 import ru.graphorismo.randomdonut.R
@@ -19,13 +20,14 @@ class MainActivity : AppCompatActivity() {
     val viewModel: MainViewModel by viewModels()
 
     lateinit var textView: TextView
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        findViewById<ImageView>(R.id.imageView)
-            .setImageResource(R.drawable.ic_baseline_cookie_24)
+        imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.drawable.ic_baseline_cookie_24)
         textView = findViewById<TextView>(R.id.textView)
         textView.setText(R.string.placeholder)
         findViewById<Button>(R.id.button)
@@ -35,7 +37,12 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.donutData.observe(this){
             if( it != null){
-                textView.setText("${it.name}\n${it.description}")
+                try{
+                    textView.setText("${it.name}\n${it.description}")
+                    imageView.load(it.imageUrl)
+                }catch (ex: Exception){
+                    Log.e("DONUT","Cant update text and image: ${ex.message}")
+                }
             }
         }
 
